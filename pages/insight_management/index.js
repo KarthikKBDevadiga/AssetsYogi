d1ba3c397ee3c257141536dab409d1ecaaa7e2ee
KarthikKBDevadiga <karthikkbdevadiga@gmail.com>
2022-01-16 18:07:03 +0530

    update Insight

--- pages/insight_management/index.js
+++ pages/insight_management/index.js
@@ -7,27 +7,77 @@ import Constants from '../../helpers/Constants'
 import MetaLayout from '../../components/ MetaLayout'
 
 import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
+import { Dialog, Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
+import { XIcon } from '@heroicons/react/outline'
+import LoadingDialog from '../../components/LoadingDialog'
+import { useRouter } from 'next/dist/client/router'
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function InsightManagementList({ data, insightTypes }) {
+export default function InsightManagementList({ data, insightTypes, token }) {
     const [sidebarOpen, setSidebarOpen] = useState(false)
     const [searchText, setSearchText] = useState('')
+    const [deleteDialog, setDeleteDialog] = useState(false)
+    const [unpublishDialog, setUnpublishDialog] = useState(false)
+    const [loadingDialog, setLoadingDialog] = useState(false)
+    const [selectedInsightId, setSelectedInsightId] = useState(-1)
+
+    const router = useRouter()
+
+    const deleteInsight = () => {
+        setDeleteDialog(false)
+        setLoadingDialog(true)
+
+        const fetch = require("node-fetch")
+
+        var myHeaders = new Headers()
+        myHeaders.append("accesstoken", token)
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        fetch(Constants.BASE_URL + "api/admin/delete_insight?id=" + selectedInsightId, requestOptions)
+            .then(res => res.json())
+            .then(
+                json => {
+                    setLoadingDialog(false)
+                    console.log(json)
+                    if (json.code == 200) {
+                        // router.back()
+                        router.reload(window.location.pathname)
+                    }
+                }
+            )
+            .catch(err => {
+                console.log('erro')
+                setLoadingDialog(false)
+                console.log(err)
+            })
+    }
+    const unpublishInsight = () => {
+        setUnpublishDialog(false)
+        // setLoadingDialog(true)
+
+        const fetch = require("node-fetch")
+
+        var myHeaders = new Headers()
+        myHeaders.append("accesstoken", token)
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+    }
     return (
         <>
-            {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-100">
-        <body class="h-full">
-        ```
-      */}
             <MetaLayout />
             <div className='font-raleway'>
                 <NavigationLayout show={sidebarOpen} setShow={setSidebarOpen} selectedId={4} />
@@ -191,29 +241,36 @@ export default function InsightManagementList({ data, insightTypes }) {
                                                                                         <div className='w-full h-px bg-black'></div>
                                                                                         <Menu.Item>
                                                                                             {({ active }) => (
-                                                                                                <a
-                                                                                                    href="#"
+                                                                                                <div
+                                                                                                    // href="#"
+                                                                                                    onClick={() => {
+                                                                                                        setSelectedInsightId(d.insight_id)
+                                                                                                        setDeleteDialog(true)
+                                                                                                    }}
                                                                                                     className={classNames(
                                                                                                         active ? 'bg-yellow-400 text-gray-900' : 'text-gray-700',
-                                                                                                        'block px-4 py-2 text-sm text-center duration-500 font-bold'
+                                                                                                        'block px-4 py-2 text-sm text-center duration-500 font-bold cursor-pointer'
                                                                                                     )}
                                                                                                 >
                                                                                                     Delete
-                                                                                                </a>
+                                                                                                </div>
                                                                                             )}
                                                                                         </Menu.Item>
                                                                                         <div className='w-full h-px bg-black'></div>
                                                                                         <Menu.Item>
                                                                                             {({ active }) => (
-                                                                                                <a
-                                                                                                    href="#"
+                                                                                                <div
+                                                                                                    onClick={() => {
+                                                                                                        setSelectedInsightId(d.insight_id)
+                                                                                                        setUnpublishDialog(true)
+                                                                                                    }}
                                                                                                     className={classNames(
                                                                                                         active ? 'bg-yellow-400 text-gray-900' : 'text-gray-700',
-                                                                                                        'block px-4 py-2 text-sm text-center duration-500 rounded-b-md font-bold'
+                                                                                                        'block px-4 py-2 text-sm text-center duration-500 rounded-b-md font-bold cursor-pointer'
                                                                                                     )}
                                                                                                 >
-                                                                                                    Unpublish
-                                                                                                </a>
+                                                                                                    {d.status == 'published' ? 'Unpublish' : 'Publish'}
+                                                                                                </div>
                                                                                             )}
                                                                                         </Menu.Item>
 
@@ -236,6 +293,159 @@ export default function InsightManagementList({ data, insightTypes }) {
                     </main>
                 </div>
             </div>
+
+            <Transition.Root show={deleteDialog} as={Fragment}>
+                <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setDeleteDialog}>
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        </Transition.Child>
+
+                        {/* This element is to trick the browser into centering the modal contents. */}
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                            &#8203;
+                        </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                                <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+                                    <button
+                                        type="button"
+                                        className="bg-white rounded-md text-gray-400 hover:text-gray-500"
+                                        onClick={() => setDeleteDialog(false)}
+                                    >
+                                        <span className="sr-only">Close</span>
+                                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                                    </button>
+                                </div>
+                                <div className="sm:flex sm:items-start">
+
+                                    <div className="mt-3 text-center sm:mt-0 sm:text-left w-full">
+                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                            Delete Insight
+                                        </Dialog.Title>
+                                        <div className='mt-4 h-px w-full bg-black'></div>
+                                        <div className="mt-2">
+                                            <p className="text-sm text-gray-500">
+                                                Are you sure, you want to delete this insight?
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="mt-5 sm:mt-8 sm:flex sm:flex-row-reverse">
+                                    <button
+                                        type="button"
+                                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 sm:ml-3 sm:w-auto sm:text-sm"
+                                        onClick={() => deleteInsight()}
+                                    >
+                                        Delete
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 sm:mt-0 sm:w-auto sm:text-sm"
+                                        onClick={() => setDeleteDialog(false)}
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </div>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition.Root>
+
+
+            <Transition.Root show={unpublishDialog} as={Fragment}>
+                <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setUnpublishDialog}>
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        </Transition.Child>
+
+                        {/* This element is to trick the browser into centering the modal contents. */}
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                            &#8203;
+                        </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                                <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+                                    <button
+                                        type="button"
+                                        className="bg-white rounded-md text-gray-400 hover:text-gray-500"
+                                        onClick={() => setUnpublishDialog(false)}
+                                    >
+                                        <span className="sr-only">Close</span>
+                                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                                    </button>
+                                </div>
+                                <div className="sm:flex sm:items-start">
+
+                                    <div className="mt-3 text-center sm:mt-0 sm:text-left w-full">
+                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                            Unpublish Insight
+                                        </Dialog.Title>
+                                        <div className='mt-4 h-px w-full bg-black'></div>
+                                        <div className="mt-2">
+                                            <p className="text-sm text-gray-500">
+                                                Are you sure, you want to unpublish this insight?
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="mt-5 sm:mt-8 sm:flex sm:flex-row-reverse">
+                                    <button
+                                        type="button"
+                                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 sm:ml-3 sm:w-auto sm:text-sm"
+                                        onClick={() => unpublishInsight()}
+                                    >
+                                        Unpublish
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 sm:mt-0 sm:w-auto sm:text-sm"
+                                        onClick={() => setUnpublishDialog(false)}
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </div>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition.Root>
+
+            <LoadingDialog showDialog={loadingDialog} setShowDialog={setLoadingDialog} />
         </>
     )
 }
@@ -281,7 +491,7 @@ export async function getServerSideProps(context) {
     console.log(data)
     return {
         props: {
-            data, insightTypes
+            data, insightTypes, token
         },
     };
 }
