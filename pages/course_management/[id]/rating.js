f374abb52c157acff12860417bc349c6606f7fbb
KarthikKBDevadiga <karthikkbdevadiga@gmail.com>
2022-01-26 18:56:31 +0530

    update

---
+++ pages/course_management/[id]/rating.js
@@ -0,0 +1,227 @@
+import cookies from 'next-cookies'
+import moment from 'moment'
+import { useState } from 'react'
+import NavigationLayout from '../../../components/NavigationLayout'
+import HeaderLayout from '../../../components/HeaderLayout'
+import Constants from '../../../helpers/Constants'
+import MetaLayout from '../../../components/ MetaLayout'
+
+import { Fragment } from 'react'
+import { Dialog, Menu, Transition, Switch } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+import LoadingDialog from '../../../components/LoadingDialog'
+import { XIcon } from '@heroicons/react/outline'
+import { useRouter } from 'next/dist/client/router'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function CourseManagementList({ data, token }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [deleteDialog, setDeleteDialog] = useState(false)
+  const [unpublishDialog, setUnpublishDialog] = useState(false)
+  const [publishDialog, setPublishDialog] = useState(false)
+  const [loadingDialog, setLoadingDialog] = useState(false)
+  const [selectedCourseId, setSelectedCourseId] = useState(-1)
+  const [searchText, setSearchText] = useState('')
+
+  const router = useRouter()
+  const [enabled, setEnabled] = useState(data.ratingstatus.allow_rating == 0 ? true : false)
+
+  return (
+    <>
+      <MetaLayout />
+      <div className='font-raleway'>
+        <NavigationLayout show={sidebarOpen} setShow={setSidebarOpen} selectedId={3} />
+        <div className="md:pl-64 flex flex-col flex-1">
+          <HeaderLayout show={sidebarOpen} setShow={setSidebarOpen} />
+
+          <main className="flex-1">
+            <div className="bg-white px-4 py-5 sm:px-6">
+              <div className="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap">
+                <div className="ml-4 mt-2 text-xl">
+                  Send the course name in the same request
+                </div>
+                <div className="ml-4 mt-2 flex-shrink-0">
+                  <Switch.Group as="div" className="flex items-center justify-between">
+                    <span className="flex-grow flex flex-col">
+                      <Switch.Label as="span" className="text-xl font-medium text-gray-900 mr-4" passive>
+                        Disable Rating for users
+                      </Switch.Label>
+                    </span>
+                    <Switch
+                      checked={enabled}
+                      onChange={setEnabled}
+                      className={classNames(
+                        enabled ? 'bg-indigo-600' : 'bg-gray-200',
+                        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                      )}
+                    >
+                      <span
+                        aria-hidden="true"
+                        className={classNames(
+                          enabled ? 'translate-x-5' : 'translate-x-0',
+                          'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+                        )}
+                      />
+                    </Switch>
+                  </Switch.Group>
+                </div>
+              </div>
+            </div>
+            <div className="px-4  ">
+              <div className="flex flex-col">
+                <div className="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                  <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+
+                      <li key='total' className="col-span-1 bg-white rounded-lg border border-black divide-y divide-gray-200">
+                        <div className="w-full flex items-center justify-between p-6 space-x-6">
+                          <div className="flex-1 truncate">
+                            <div className="flex items-center space-x-3">
+                              <h3 className="text-tcolor text-xl font-medium truncate align-center ml-auto mr-auto">Total Rating</h3>
+
+                            </div>
+                            <p className="mt-1 text-gray-500 text-2xl font-bold truncate text-center">{data.totalrating}</p>
+                          </div>
+                        </div>
+                      </li>
+                      <li key='average' className="col-span-1 bg-white rounded-lg border border-black divide-y divide-gray-200">
+                        <div className="w-full flex items-center justify-between p-6 space-x-6">
+                          <div className="flex-1 truncate">
+                            <div className="flex items-center space-x-3">
+                              <h3 className="text-tcolor text-xl font-medium truncate align-center ml-auto mr-auto">Average Rating</h3>
+
+                            </div>
+                            <p className="mt-1 text-gray-500 text-2xl font-bold truncate text-center">{data.averagerating}</p>
+                          </div>
+                        </div>
+                      </li>
+                      <li key='highest' className="col-span-1 bg-white rounded-lg border border-black divide-y divide-gray-200">
+                        <div className="w-full flex items-center justify-between p-6 space-x-6">
+                          <div className="flex-1 truncate">
+                            <div className="flex items-center space-x-3">
+                              <h3 className="text-tcolor text-xl font-medium truncate align-center ml-auto mr-auto">Highest Rating</h3>
+
+                            </div>
+                            <p className="mt-1 text-gray-500 text-2xl font-bold truncate text-center">{data.highestrating}</p>
+                          </div>
+                        </div>
+                      </li>
+                      <li key='lowest' className="col-span-1 bg-white rounded-lg border border-black divide-y divide-gray-200">
+                        <div className="w-full flex items-center justify-between p-6 space-x-6">
+                          <div className="flex-1 truncate">
+                            <div className="flex items-center space-x-3">
+                              <h3 className="text-tcolor text-xl font-medium truncate align-center ml-auto mr-auto">Lowest Rating</h3>
+
+                            </div>
+                            <p className="mt-1 text-gray-500 text-2xl font-bold truncate text-center">{data.lowestrating}</p>
+                          </div>
+                        </div>
+                      </li>
+
+                    </ul>
+                    <div className=" border-b border-gray-200 sm:rounded-lg mt-4">
+                      <table className="min-w-full divide-y divide-gray-900">
+                        <thead className="">
+                          <tr>
+                            <th
+                              scope="col"
+                              className="px-2 py-3 text-left font-bold uppercase tracking-wider text-center"
+                            >
+                              S. No
+                            </th>
+                            <th
+                              scope="col"
+                              className="px-2 py-3 text-left font-bold uppercase tracking-wider text-center"
+                            >
+                              User Name
+                            </th>
+                            <th
+                              scope="col"
+                              className="px-2 py-3 text-left font-bold uppercase tracking-wider text-center"
+                            >
+                              Rating
+                            </th>
+                            <th
+                              scope="col"
+                              className="px-2 py-3 text-left  font-bold uppercase tracking-wider text-center"
+                            >
+                              User Since
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody className="bg-white divide-y divide-gray-300 text-sm">
+                          {
+                            data.ratings.map((r, i) => {
+                              return <tr key={r.id}>
+                                <td className="p-2 whitespace-nowrap text-center">
+                                  {i + 1}
+                                </td>
+                                <td className="p-2 whitespace-nowrap text-center">
+                                  {r.user_name}
+                                </td>
+                                <td className="p-2 whitespace-nowrap text-center">
+                                  {r.ratings}
+                                </td>
+                                <td className="p-2 whitespace-nowrap text-center">
+                                  {moment(r.created_at).format('DD MMM YYYY')}
+                                </td>
+                              </tr>
+                            })
+                          }
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+
+    </>
+  )
+}
+export async function getServerSideProps(context) {
+  const { token } = cookies(context)
+  const courseId = context.params.id
+
+  if (token == null || token == '') {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login"
+      }
+    }
+  }
+
+  const fetch = require("node-fetch")
+
+  const body = {
+    "status": 1,
+    "course_id": courseId
+  }
+  const data = await fetch(Constants.BASE_URL + "api/admin/get_course_rating", {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+      "accesstoken": token
+    }
+  })
+    .then(res => res.json())
+    .then(json => json)
+    .catch(err => {
+      console.log(err)
+    })
+  console.log(data)
+  return {
+    props: {
+      data, token
+    },
+  };
+}
--- pages/course_management/index.js
+++ pages/course_management/index.js
@@ -23,6 +23,8 @@ export default function CourseManagementList({ data, token }) {
     const [deleteDialog, setDeleteDialog] = useState(false)
     const [unpublishDialog, setUnpublishDialog] = useState(false)
     const [publishDialog, setPublishDialog] = useState(false)
+    const [announcement, setAnnouncement] = useState('')
+    const [announcementDialog, setAnnouncementDialog] = useState(false)
     const [loadingDialog, setLoadingDialog] = useState(false)
     const [selectedCourseId, setSelectedCourseId] = useState(-1)
     const [searchText, setSearchText] = useState('')
@@ -113,6 +115,42 @@ export default function CourseManagementList({ data, token }) {
                     if (json.code == 200) {
                         // router.back()
                         router.reload(window.location.pathname)
+                    }
+                }
+            )
+            .catch(err => {
+                console.log('erro')
+                setLoadingDialog(false)
+                console.log(err)
+            })
+    }
+
+    const announceCourse = () => {
+        setAnnouncementDialog(false)
+        setLoadingDialog(true)
+        const fetch = require("node-fetch")
+
+        const body = {
+            "course_id": selectedCourseId,
+            "announcement": announcement
+        }
+        fetch(Constants.BASE_URL + "api/admin/add_course_annoucement", {
+            method: "post",
+            body: JSON.stringify(body),
+            headers: {
+                // "Content-Type": "application/json",
+                "accesstoken": token
+            }
+        })
+            .then(res => res.json()
+            )
+            .then(
+                json => {
+                    setLoadingDialog(false)
+                    console.log(json)
+                    if (json.code == 200) {
+                        // router.back()
+                        // router.reload(window.location.pathname)
                     }
                 }
             )
@@ -369,7 +407,7 @@ export default function CourseManagementList({ data, token }) {
                                                                                         <Menu.Item>
                                                                                             {({ active }) => (
                                                                                                 <a
-                                                                                                    href="#"
+                                                                                                    href={"/course_management/" + d.course_id + "/rating"}
                                                                                                     className={classNames(
                                                                                                         active ? 'bg-yellow-400 text-gray-900' : 'text-gray-700',
                                                                                                         'block px-4 py-2 text-sm text-center duration-500 font-bold'
@@ -382,15 +420,18 @@ export default function CourseManagementList({ data, token }) {
                                                                                         <div className='w-full h-px bg-black'></div>
                                                                                         <Menu.Item>
                                                                                             {({ active }) => (
-                                                                                                <a
-                                                                                                    href="#"
+                                                                                                <div
+                                                                                                    onClick={() => {
+                                                                                                        setSelectedCourseId(d.course_id)
+                                                                                                        setAnnouncementDialog(true)
+                                                                                                    }}
                                                                                                     className={classNames(
                                                                                                         active ? 'bg-yellow-400 text-gray-900' : 'text-gray-700',
-                                                                                                        'block px-4 py-2 text-sm text-center duration-500 font-bold'
+                                                                                                        'block px-4 py-2 text-sm text-center duration-500 font-bold cursor-pointer'
                                                                                                     )}
                                                                                                 >
                                                                                                     Announcements
-                                                                                                </a>
+                                                                                                </div>
                                                                                             )}
                                                                                         </Menu.Item>
                                                                                         <div className='w-full h-px bg-black'></div>
@@ -671,6 +712,96 @@ export default function CourseManagementList({ data, token }) {
                                         type="button"
                                         className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 sm:mt-0 sm:w-auto sm:text-sm"
                                         onClick={() => setPublishDialog(false)}
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </div>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition.Root>
+
+
+            <Transition.Root show={announcementDialog} as={Fragment}>
+                <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setAnnouncementDialog}>
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        </Transition.Child>
+
+                        {/* This element is to trick the browser into centering the modal contents. */}
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                            &#8203;
+                        </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                                <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
+                                    <button
+                                        type="button"
+                                        className="bg-white rounded-md text-gray-400 hover:text-gray-500"
+                                        onClick={() => setAnnouncementDialog(false)}
+                                    >
+                                        <span className="sr-only">Close</span>
+                                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                                    </button>
+                                </div>
+                                <div className="sm:flex sm:items-start">
+
+                                    <div className="mt-3 text-center sm:mt-0 sm:text-left w-full">
+                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                            Announcements
+                                        </Dialog.Title>
+                                        <div className='mt-4 h-px w-full bg-black'></div>
+                                        <div className="mt-2">
+                                            <div>
+                                                <label htmlFor="comment" className="block text-sm font-medium text-gray-700">
+                                                    Announcements
+                                                </label>
+                                                <div className="mt-1">
+                                                    <textarea
+                                                        onChange={(event) => {
+                                                            setAnnouncement(event.target.value)
+                                                        }}
+                                                        rows={4}
+                                                        name="announcement"
+                                                        id="announcement"
+                                                        className="shadow-sm block w-full sm:text-sm border border-bcolor rounded-md px-4 py-2"
+                                                        defaultValue={''}
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="mt-5 sm:mt-8 sm:flex sm:flex-row-reverse">
+                                    <button
+                                        type="button"
+                                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 sm:ml-3 sm:w-auto sm:text-sm"
+                                        onClick={() => announceCourse()}
+                                    >
+                                        Publish
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:text-gray-500 sm:mt-0 sm:w-auto sm:text-sm"
+                                        onClick={() => setAnnouncementDialog(false)}
                                     >
                                         Cancel
                                     </button>
